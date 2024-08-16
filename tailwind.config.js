3a73a1f0e4527a0bf3354202ecae8bb244f39a1f
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-17 00:59:33 +0600

    change added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
@@ -14,7 +15,10 @@ module.exports = {
       },
     },
   },
-  plugins: [require("./plugins/openVariant"), require("./plugins/animationDelay")],
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+  ],
   vairants: {
     extend: {
       display: ["group-open"],
